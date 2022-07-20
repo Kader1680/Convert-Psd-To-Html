2471f013c5efc3485c191919203f32e70f4f112c
Kader1680 <ouldhenniabaghdad@gmail.com>
2022-07-20 08:09:02 +0100

    update slider

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,25 @@ app.forEach(li => {
 });
 
 
+// slider img
+const swiper = new Swiper('.swiper', {
+
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    
+    navigation: {
+        // nextEl: '.swiper-button-next',
+        // prevEl: '.swiper-button-prev',
+        nextEl: '.next',
+        prevEl: '.prev',
+    },
+
+    
+});
 
 
 
